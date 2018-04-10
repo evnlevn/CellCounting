684e327e6465e7e08e0d22cc8e521f481dc1cb29
denzf <35637698+denzf@users.noreply.github.com>
2018-04-10 18:35:21 -0400

    Update ProjectScript.m

--- ProjectScript.m
+++ ProjectScript.m
@@ -9,16 +9,6 @@ path1 = 'G:\Denzel\RPI\Year 2\Spring 2018\Classes - Spring 2018\BMED 6460 - Biol
 cd(path1);
 addpath(genpath(path1));
 %
-%
-tic
-% Extracting OCT Information
-% 
-% Open OCT file with 7zip. 
-% Note: Name will need to be changed every time analysis is conducted
-%
-addpath(genpath('C:\Users\OCT\Desktop\OCT Scripts\'));
-
-octFile = OCTFileOpen('D0-Sample1.oct');
-%
 % Extract intensity from OCT file
-intensity = OCTFileGetIntensity(octFile); % Output is in form [z,x,y]
+intensity = load('D1S1_intensity.mat'); % Output is in form [z,x,y]
+intensity = permute(intensity,[3,1,2])
